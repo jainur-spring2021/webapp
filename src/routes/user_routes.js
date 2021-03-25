3e84c7ecc14b935665821001d381126001edc9cd
urvashijain18 <jain.ur@husky.neu.edu>
2021-03-25 01:46:57 -0400

    update Routes

--- src/routes/user_routes.js
+++ src/routes/user_routes.js
@@ -31,5 +31,8 @@ module.exports = app => {
     //Delete an image
     router.delete("/books/:bookId/image/:imageId", images.deleteImage);
 
-    app.use('/api/', router);
+    //Delete an image
+    router.get("/", function (req, res) {res.send('Welcome!')});
+
+    app.use('/', router);
   };
